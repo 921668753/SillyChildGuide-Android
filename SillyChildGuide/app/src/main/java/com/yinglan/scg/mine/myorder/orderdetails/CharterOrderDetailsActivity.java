5d3f2cb7eadd295cf40d01bc5eafdf42f66a019e
921668753 <921668753@qq.com>
2018-09-10 11:14:17 +0800

    fix BUG

--- SillyChildGuide/app/src/main/java/com/yinglan/scg/mine/myorder/orderdetails/CharterOrderDetailsActivity.java
+++ SillyChildGuide/app/src/main/java/com/yinglan/scg/mine/myorder/orderdetails/CharterOrderDetailsActivity.java
@@ -1,15 +1,117 @@
 package com.yinglan.scg.mine.myorder.orderdetails;
 
+import android.support.v7.widget.RecyclerView;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 import com.common.cklibrary.common.BaseActivity;
+import com.common.cklibrary.common.BindView;
+import com.common.cklibrary.common.ViewInject;
+import com.common.cklibrary.utils.myview.ChildListView;
+import com.common.cklibrary.utils.myview.NoScrollGridView;
+import com.common.cklibrary.utils.myview.WebViewLayout;
 import com.yinglan.scg.R;
+import com.yinglan.scg.loginregister.LoginActivity;
 
 /**
  * 线路订单详情
  */
-public class CharterOrderDetailsActivity extends BaseActivity {
+public class CharterOrderDetailsActivity extends BaseActivity implements CharterOrderDetailsContract.View {
+
+    @BindView(id = R.id.tv_title)
+    private TextView tv_title;
+
+    @BindView(id = R.id.tv_orderPrice)
+    private TextView tv_orderPrice;
+
+    @BindView(id = R.id.tv_demand)
+    private TextView tv_demand;
+
+    @BindView(id = R.id.tv_time)
+    private TextView tv_time;
+
+    @BindView(id = R.id.tv_serviceTime)
+    private TextView tv_serviceTime;
+
+
+    @BindView(id = R.id.tv_placeDeparture)
+    private TextView tv_placeDeparture;
+
+    @BindView(id = R.id.tv_deliveredAirport)
+    private TextView tv_deliveredAirport;
+
+    @BindView(id = R.id.tv_reserveRequirements)
+    private TextView tv_reserveRequirements;
+
+    @BindView(id = R.id.tv_orderNumber)
+    private TextView tv_orderNumber;
+
+    @BindView(id = R.id.web_dueThat)
+    private WebViewLayout web_dueThat;
+
+    @BindView(id = R.id.clv_income)
+    private ChildListView clv_income;
+
+    @BindView(id = R.id.tv_aggregate)
+    private TextView tv_aggregate;
+
+    @BindView(id = R.id.web_descriptionThat)
+    private WebViewLayout web_descriptionThat;
+
+    @BindView(id = R.id.img_head)
+    private ImageView img_head;
+
+    @BindView(id = R.id.tv_nickName)
+    private TextView tv_nickName;
+
+    @BindView(id = R.id.img_satisfactionLevel)
+    private ImageView img_satisfactionLevel;
+
+    @BindView(id = R.id.tv_content)
+    private TextView tv_content;
+
+    @BindView(id = R.id.gv_imgComments)
+    private NoScrollGridView gv_imgComments;
+
+    @BindView(id = R.id.tv_time1)
+    private TextView tv_time1;
+
+    @BindView(id = R.id.et_evaluateGuest)
+    private EditText et_evaluateGuest;
+
+    @BindView(id = R.id.recyclerView)
+    private RecyclerView recyclerView;
+
+    @BindView(id = R.id.tv_submitAudit, click = true)
+    private TextView tv_submitAudit;
+
 
     @Override
     public void setRootView() {
         setContentView(R.layout.activity_charterorderdetails);
+    }
+
+    @Override
+    public void setPresenter(CharterOrderDetailsContract.Presenter presenter) {
+        mPresenter = presenter;
+    }
+
+    @Override
+    public void getSuccess(String success, int flag) {
+        dismissLoadingDialog();
+    }
+
+    @Override
+    public void errorMsg(String msg, int flag) {
+        dismissLoadingDialog();
+        if (isLogin(msg)) {
+            showActivity(aty, LoginActivity.class);
+            if (flag == 0) {
+                finish();
+            }
+            return;
+        }
+        ViewInject.toast(msg);
     }
 }
---
+++ SillyChildGuide/app/src/main/java/com/yinglan/scg/mine/myorder/orderdetails/CharterOrderDetailsContract.java
@@ -0,0 +1,31 @@
+package com.yinglan.scg.mine.myorder.orderdetails;
+
+import android.content.Context;
+
+import com.common.cklibrary.common.BasePresenter;
+import com.common.cklibrary.common.BaseView;
+
+/**
+ * Created by ruitu on 2016/9/24.
+ */
+
+public interface CharterOrderDetailsContract {
+    interface Presenter extends BasePresenter {
+        /**
+         * 获取订单信息
+         */
+        void getChartOrder(Context context, String status, int page);
+
+
+        /**
+         * 获取会员登录状态
+         */
+        void getIsLogin(Context context, int flag);
+    }
+
+    interface View extends BaseView<Presenter, String> {
+    }
+
+}
+
+
